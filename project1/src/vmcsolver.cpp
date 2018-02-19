736d1ecc0edcd1af702b0264a674f585e50c603d
Bendik Samseth <b.samseth@gmail.com>
2018-02-19 11:40:12 +0100

    Premature optimization is the root of all evil.
    Migrate from template based options to arguments.
    No speedup observed, would only make life hard when trying
    to run multiple different solver configurations.

---
+++ project1/src/vmcsolver.cpp
@@ -0,0 +1,233 @@
+#include <random>
+#include <cassert>
+
+#include "vmcsolver.hpp"
+
+namespace VMC {
+
+std::mt19937_64 rand_gen;
+std::uniform_real_distribution<double> unif(0, 1);
+std::uniform_real_distribution<double> centered(-0.5, 0.5);
+std::normal_distribution<double> rnorm(0, 1);
+
+VMCSolver::VMCSolver(const VMCConfiguration &config) : _config(config) {}
+
+double VMCSolver::V_ext(const arma::mat &R) const {
+    double pot = 0;
+    for (int i = 0; i < R.n_rows; ++i) {
+        if (_config.ho_type == HOType::ELLIPTICAL and _config.dims == Dimensions::DIM_3) {
+            pot += _config.omega_ho * (R(i, 0)*R(i, 0) + R(i, 1)*R(i, 1))
+                 + _config.omega_z  *  R(i, 2)*R(i, 2);
+        } else {
+            pot += _config.omega_ho * arma::dot(R.row(i), R.row(i));
+        }
+    }
+    return 0.5 * pot;
+}
+
+double VMCSolver::V_int(const arma::mat &R) const {
+    if (_config.interaction == InteractionType::OFF) return 0;
+    for (int i = 0; i < R.n_rows; ++i) {
+        for (int j = i + 1; j < R.n_rows; ++j) {
+            auto r_ij = R.row(i) - R.row(j);
+            if (arma::dot(r_ij, r_ij) <= _config.a * _config.a)
+                return std::numeric_limits<double>::max();
+        }
+    }
+    return 0;
+}
+
+double VMCSolver::Psi_f(const arma::mat &R) const {
+    if (_config.interaction == InteractionType::OFF) return 1;
+    double f = 1;
+    for (int i = 0; i < R.n_rows; ++i) {
+        for (int j = i + 1; j < R.n_rows; ++j) {
+            double r_ij = arma::norm(R.row(i) - R.row(j));
+            if (r_ij <= _config.a)
+                return 0;
+            f *= (1 - _config.a / r_ij);
+        }
+    }
+    return f;
+}
+
+double VMCSolver::Psi_g(const arma::mat &R) const {
+    double g = 0;
+    for (int i = 0; i < R.n_rows; ++i) {
+        g += arma::dot(R.row(i), R.row(i));
+    }
+    return std::exp(-_alpha * g);
+}
+
+double VMCSolver::Psi(const arma::mat &R) const {
+    return Psi_g(R) * Psi_f(R);
+}
+
+double VMCSolver::E_kinetic(arma::mat &R) const {
+
+    double Ek = - 2 * (_config.n_particles * _config.dims) * Psi(R);
+
+    for (int i = 0; i < R.n_rows; ++i) {
+        for (int d = 0; d < R.n_cols; ++d) {
+            auto temp = R(i, d);
+            R(i, d) = temp + _config.h;
+            Ek += Psi(R);      // Psi(R + h)
+            R(i, d) = temp - _config.h;
+            Ek += Psi(R);      // Psi(R - h)
+            R(i, d) = temp;
+        }
+    }
+    return -0.5 * Ek * _config.h2;
+}
+
+double VMCSolver::E_local(arma::mat &R) const {
+    if (_config.acceleration == AnalyticAcceleration::OFF)
+        return E_kinetic(R) / Psi(R) + V_ext(R) + V_int(R);
+
+    double E_L = 0;
+    for (int k = 0; k < R.n_rows; ++k) {
+
+        // r_k = R(k, :)
+        // r_k_skewed = {x_k, y_k, _beta * z_k} if Dims == 3
+        //               r_k                    otherwise.
+        const auto r_k = R.row(k);
+        arma::rowvec r_k_skewed = R.row(k);
+        if (_config.dims == Dimensions::DIM_3)
+            r_k_skewed(2) *= _beta;
+
+
+        // First term, no interaction.
+        E_L += 2*_alpha * (2*_alpha * arma::dot(r_k_skewed, r_k_skewed)
+                           - (_config.dims == Dimensions::DIM_3 ? 2 + _beta :
+                                                                  _config.dims));
+
+
+        // Remaining terms are only for interaction, may be removed by compiler.
+        if (_config.interaction == InteractionType::OFF)
+            continue;
+
+        arma::rowvec term (R.n_cols);
+        for (int j = 0; j < R.n_rows; ++j) {  // j != k.
+            if (j == k) continue;
+
+            const auto r_kj = r_k - R.row(j);
+            const double r_kj_norm = arma::norm(r_kj);
+            const double r_kj_2 = r_kj_norm * r_kj_norm;
+
+            term += r_kj * (_config.a / (r_kj_2 * (r_kj_norm - _config.a)));
+
+            E_L += _config.a * (_config.a - 2 * r_kj_norm) / (r_kj_2 * (r_kj_norm - _config.a) * (r_kj_norm - _config.a))
+                    + 2 * _config.a / (r_kj_2 * (r_kj_norm - _config.a));
+
+            for (int i = 0; i < R.n_rows; ++i) {  // i != k.
+                if (i == k) continue;
+
+                const auto r_ki = r_k - R.row(i);
+                const double r_ki_norm = arma::norm(r_ki);
+                const double r_ki_2 = r_ki_norm * r_ki_norm;
+
+                E_L += arma::dot(r_ki, r_kj) * (_config.a * _config.a / (r_ki_2 * r_kj_2 * (r_ki_norm - _config.a) * (r_kj_norm - _config.a)));
+            }
+        }
+        E_L -= 4 * _alpha * arma::dot(r_k_skewed, term);
+    }
+    return V_ext(R) + V_int(R) - 0.5 * E_L;
+}
+
+Results VMCSolver::run_MC(const int n_cycles) const {
+    arma::mat R_old (_config.n_particles, _config.dims);
+    arma::mat R_new (arma::size(R_old));
+    double E_sum = 0, E2_sum = 0;
+
+    // Random initial starting point.
+    for (int i = 0; i < _config.n_particles; ++i) {
+        for (int d = 0; d < _config.dims; ++d) {
+            R_old(i, d) = R_new(i, d) = _config.step_length * centered(rand_gen);
+        }
+    }
+
+    int accepted_moves = 0;
+    for (int cycle = 1; cycle <= n_cycles; ++cycle) {
+        double Psi_old = Psi(R_old);
+        for (int i = 0; i < _config.n_particles; ++i) {
+            // Move particle i slightly.
+            for (int d = 0; d < _config.dims; ++d) {
+                R_new(i, d) = R_old(i, d) + _config.step_length * centered(rand_gen);
+            }
+            double Psi_new = Psi(R_new);
+
+            // New move accepted?
+            if (unif(rand_gen) <= (Psi_new * Psi_new) / (Psi_old * Psi_old)) {
+                accepted_moves++;
+                // Update old <- new.
+                Psi_old = Psi_new;
+                for (int d = 0; d < _config.dims; ++d) {
+                    R_old(i, d) = R_new(i, d);
+                }
+            }
+            else {
+                // Restore new <- old.
+                for (int d = 0; d < _config.dims; ++d) {
+                    R_new(i, d) = R_old(i, d);
+                }
+            }
+
+            // Update averages.
+            double E = E_local(R_new);
+            E_sum += E;
+            E2_sum += E*E;
+
+        }
+    }
+    printf("Accepted rate: %.0f %%\n", 100 * accepted_moves / (double) (n_cycles * _config.n_particles));
+    double energy = E_sum / (n_cycles * _config.n_particles);
+    double energy_squared = E2_sum / (n_cycles * _config.n_particles);
+    double variance = energy_squared - energy*energy;
+    return {energy, energy_squared, variance, _alpha, _beta};
+}
+
+Results VMCSolver::vmc(const int n_cycles,
+            std::ostream &out,
+            const double alpha_min,
+            const double alpha_max,
+            const double alpha_step,
+            const double beta_min,
+            const double beta_max,
+            const double beta_step) {
+
+    // Used to store best results.
+    Results best = {0, 0, std::numeric_limits<double>::max(), 0, 0};
+
+    // Define variational space.
+    const int n_alpha = (alpha_max - alpha_min) / alpha_step + 1;
+    const int n_beta  = (beta_max  - beta_min ) / beta_step  + 1;
+    const auto alpha = arma::linspace<arma::vec>(alpha_min, alpha_max, n_alpha);
+    const auto beta  = arma::linspace<arma::vec>(beta_min, beta_max, n_beta);
+
+    // Write header to stream.
+    out << "# alpha beta <E> <E^2>\n";
+
+    // For every combination of parameters,
+    // write the results of MC to the stream.
+    for (int a = 0; a < n_alpha; ++a) {
+        _alpha = alpha(a);
+        for (int b = 0; b < n_beta; ++b) {
+            _beta = beta(b);
+            Results res = run_MC(n_cycles);
+            out << _alpha << " "
+                << _beta  << " "
+                << res.E  << " "
+                << res.E2 << "\n";
+
+            // New best parameter choice?
+            if (res.variance < best.variance)
+                best = res;
+        }
+    }
+    // Flush when you are done.
+    out << std::flush;
+
+    return best;
+}
+
+}  // namespace VMC
---
+++ project1/src/vmcsolver.hpp
@@ -0,0 +1,80 @@
+#ifndef VMCSOLVER_HPP
+#define VMCSOLVER_HPP
+
+#include <ostream>
+#include <armadillo>
+
+namespace VMC {
+
+namespace Constants {
+} // namespace Constants
+
+enum Dimensions {
+    DIM_1 = 1, DIM_2, DIM_3
+};
+enum class HOType {
+    SYMMETRIC, ELLIPTICAL
+};
+enum class InteractionType {
+    OFF = 0, ON
+};
+
+enum class AnalyticAcceleration {
+    OFF = 0, ON
+};
+
+struct Results {
+    double E, E2, variance, alpha, beta;
+};
+
+struct VMCConfiguration {
+    int n_particles;
+    Dimensions dims;
+    HOType ho_type;
+    InteractionType interaction;
+    AnalyticAcceleration acceleration;
+    double omega_ho;
+    double omega_z;
+    double a;
+    double h;
+    double h2;
+    double step_length;
+};
+
+class VMCSolver {
+private:
+    const VMCConfiguration _config;
+    double _alpha = 0.5, _beta = 1;
+
+public:
+    VMCSolver(const VMCConfiguration &config);
+
+    double V_ext(const arma::mat &R) const;
+
+    double V_int(const arma::mat &R) const;
+
+    double Psi_f(const arma::mat &R) const;
+
+    double Psi_g(const arma::mat &R) const;
+
+    double Psi(const arma::mat &R) const;
+
+    double E_kinetic(arma::mat &R) const;
+
+    double E_local(arma::mat &R) const;
+
+    Results run_MC(const int n_cycles) const;
+
+    Results vmc(const int n_cycles,
+                std::ostream &out,
+                const double alpha_min,
+                const double alpha_max,
+                const double alpha_step = 1,
+                const double beta_min = 1,
+                const double beta_max = 1,
+                const double beta_step = 1);
+};
+
+}  // namespace VMC
+
+#endif // VMCSOLVER_HPP
