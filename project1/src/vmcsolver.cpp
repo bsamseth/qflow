bcebf6d5958c021f5dedeb447334979c7eb338c4
Bendik Samseth <b.samseth@gmail.com>
2018-02-20 13:31:33 +0100

    Refactor the column major indexing.
    Armadillo stores matrices in a column major order,
    so should work on columns as much as possible.
    Now each particle is one column in the R matrices, with
    each row corresponding to a dimension.

--- project1/src/vmcsolver.cpp
+++ project1/src/vmcsolver.cpp
@@ -11,8 +11,8 @@ std::uniform_real_distribution<double> centered(-0.5, 0.5);
 std::normal_distribution<double> rnorm(0, 1);
 
 VMCSolver::VMCSolver(const VMCConfiguration &config) : _config(config) {
-    R_old = arma::zeros<arma::mat>(_config.n_particles, _config.dims);
-    R_new = arma::zeros<arma::mat>(_config.n_particles, _config.dims);
+    R_old = arma::zeros<arma::mat>(_config.dims, _config.n_particles);
+    R_new = arma::zeros<arma::mat>(_config.dims, _config.n_particles);
     dist  = arma::zeros<arma::mat>(_config.n_particles, _config.n_particles);
 }
 
@@ -23,7 +23,7 @@ void VMCSolver::initialize_distance_matrix(const arma::mat &R) {
 
     for (int i = 0; i < _config.n_particles; ++i) {
         for (int j = i + 1; j < _config.n_particles; ++j) {
-            dist(i, j) = arma::norm(R.row(i) - R.row(j));
+            dist(i, j) = arma::norm(R.col(i) - R.col(j));
         }
     }
 }
@@ -32,23 +32,23 @@ void VMCSolver::update_distance_matrix(int particle, const arma::mat &R) {
 
     // Update row particle. Only work on upper half of matrix.
     for (int other = particle + 1; other < _config.n_particles; ++other) {
-        dist(particle, other) = arma::norm(R.row(particle) - R.row(other));
+        dist(particle, other) = arma::norm(R.col(particle) - R.col(other));
     }
 
     // Update col particle. Only work on upper half of matrix.
     for (int other = 0; other < particle; ++other) {
-        dist(other, particle) = arma::norm(R.row(other) - R.row(particle));
+        dist(other, particle) = arma::norm(R.col(other) - R.col(particle));
     }
 }
 
 double VMCSolver::V_ext(const arma::mat &R) const {
     double pot = 0;
-    for (unsigned i = 0; i < R.n_rows; ++i) {
+    for (int i = 0; i < _config.n_particles; ++i) {
         if (_config.ho_type == HOType::ELLIPTICAL and _config.dims == Dimensions::DIM_3) {
-            pot += _config.omega_ho * (R(i, 0)*R(i, 0) + R(i, 1)*R(i, 1))
-                 + _config.omega_z  *  R(i, 2)*R(i, 2);
+            pot += _config.omega_ho * (R(0, i)*R(0, i) + R(1, i)*R(1, i))
+                 + _config.omega_z  *  R(2, i)*R(2, i);
         } else {
-            pot += _config.omega_ho * arma::dot(R.row(i), R.row(i));
+            pot += _config.omega_ho * arma::dot(R.col(i), R.col(i));
         }
     }
     return 0.5 * pot;
@@ -56,8 +56,8 @@ double VMCSolver::V_ext(const arma::mat &R) const {
 
 double VMCSolver::V_int(const arma::mat &R) const {
     if (_config.interaction == InteractionType::OFF) return 0;
-    for (unsigned i = 0; i < R.n_rows; ++i) {
-        for (unsigned j = i + 1; j < R.n_rows; ++j) {
+    for (unsigned i = 0; i < _config.n_particles; ++i) {
+        for (unsigned j = i + 1; j < _config.n_particles; ++j) {
             if (dist(i, j) <= _config.a)
                 return std::numeric_limits<double>::max();
         }
@@ -68,8 +68,8 @@ double VMCSolver::V_int(const arma::mat &R) const {
 double VMCSolver::Psi_f(const arma::mat &R) const {
     if (_config.interaction == InteractionType::OFF) return 1;
     double f = 1;
-    for (unsigned i = 0; i < R.n_rows; ++i) {
-        for (unsigned j = i + 1; j < R.n_rows; ++j) {
+    for (int i = 0; i < _config.n_particles; ++i) {
+        for (int j = i + 1; j < _config.n_particles; ++j) {
             const double r_ij = dist(i, j);
             if (r_ij <= _config.a)
                 return 0;
@@ -81,8 +81,8 @@ double VMCSolver::Psi_f(const arma::mat &R) const {
 
 double VMCSolver::Psi_g(const arma::mat &R) const {
     double g = 0;
-    for (unsigned i = 0; i < R.n_rows; ++i) {
-        g += arma::dot(R.row(i), R.row(i));
+    for (unsigned i = 0; i < _config.n_particles; ++i) {
+        g += arma::dot(R.col(i), R.col(i));
     }
     return std::exp(-_alpha * g);
 }
@@ -95,28 +95,28 @@ double VMCSolver::E_kinetic(arma::mat &R) {
 
     double Ek = - 2 * (_config.n_particles * _config.dims) * Psi(R);
 
-    for (unsigned i = 0; i < R.n_rows; ++i) {
-        for (unsigned d = 0; d < R.n_cols; ++d) {
+    for (int i = 0; i < _config.n_particles; ++i) {
+        for (int d = 0; d < _config.dims; ++d) {
 
             // Storing a temporary instead of adding/subtracting away
             // the changes should help avoid accumulating rounding errors.
-            const auto temp = R(i, d);
+            const auto temp = R(d, i);
 
             // Psi(R) depends on the dist matrix being updated after changes.
             // Only needed if interaction is on.
 
-            R(i, d) = temp + _config.h;
+            R(d, i) = temp + _config.h;
             if (_config.interaction == InteractionType::ON) update_distance_matrix(i, R);
 
             Ek += Psi(R);      // Psi(R + h)
 
-            R(i, d) = temp - _config.h;
+            R(d, i) = temp - _config.h;
             if (_config.interaction == InteractionType::ON) update_distance_matrix(i, R);
 
             Ek += Psi(R);      // Psi(R - h)
 
             // Reset distance.
-            R(i, d) = temp;
+            R(d, i) = temp;
             if (_config.interaction == InteractionType::ON) update_distance_matrix(i, R);
         }
     }
@@ -128,13 +128,13 @@ double VMCSolver::E_local(arma::mat &R) {
         return E_kinetic(R) / Psi(R) + V_ext(R) + V_int(R);
 
     double E_L = 0;
-    for (unsigned k = 0; k < R.n_rows; ++k) {
+    for (int k = 0; k < _config.n_particles; ++k) {
 
-        // r_k = R(k, :)
+        // r_k = R(:, k)
         // r_k_skewed = {x_k, y_k, _beta * z_k} if Dims == 3
         //               r_k                    otherwise.
-        const auto r_k = R.row(k);
-        arma::rowvec r_k_skewed = R.row(k);
+        const auto r_k = R.col(k);
+        arma::vec r_k_skewed = R.col(k);
         if (_config.dims == Dimensions::DIM_3)
             r_k_skewed(2) *= _beta;
 
@@ -150,11 +150,11 @@ double VMCSolver::E_local(arma::mat &R) {
             continue;
 
 
-        arma::rowvec term (R.n_cols);
-        for (unsigned j = 0; j < R.n_rows; ++j) {  // j != k.
+        arma::vec term (_config.dims);
+        for (int j = 0; j < _config.n_particles; ++j) {  // j != k.
             if (j == k) continue;
 
-            const auto r_kj = r_k - R.row(j);
+            const auto r_kj = r_k - R.col(j);
             const double r_kj_norm = dist(std::min(k, j), std::max(k, j));
             const double r_kj_2 = r_kj_norm * r_kj_norm;
 
@@ -163,10 +163,10 @@ double VMCSolver::E_local(arma::mat &R) {
             E_L += _config.a * (_config.a - 2 * r_kj_norm) / (r_kj_2 * (r_kj_norm - _config.a) * (r_kj_norm - _config.a))
                     + 2 * _config.a / (r_kj_2 * (r_kj_norm - _config.a));
 
-            for (unsigned i = 0; i < R.n_rows; ++i) {  // i != k.
+            for (int i = 0; i < _config.n_particles; ++i) {  // i != k.
                 if (i == k) continue;
 
-                const auto r_ki = r_k - R.row(i);
+                const auto r_ki = r_k - R.col(i);
                 const double r_ki_norm = dist(std::min(k, i), std::max(k, i));
                 const double r_ki_2 = r_ki_norm * r_ki_norm;
 
@@ -179,14 +179,14 @@ double VMCSolver::E_local(arma::mat &R) {
 }
 
 Results VMCSolver::run_MC(const int n_cycles) {
-    arma::mat R_old (_config.n_particles, _config.dims);
-    arma::mat R_new (arma::size(R_old));
+    arma::mat R_old (_config.dims, _config.n_particles);
+    arma::mat R_new (_config.dims, _config.n_particles);
     double E_sum = 0, E2_sum = 0;
 
     // Random initial starting point.
     for (int i = 0; i < _config.n_particles; ++i) {
         for (int d = 0; d < _config.dims; ++d) {
-            R_old(i, d) = R_new(i, d) = _config.step_length * centered(rand_gen);
+            R_old(d, i) = R_new(d, i) = _config.step_length * centered(rand_gen);
         }
     }
 
@@ -198,7 +198,7 @@ Results VMCSolver::run_MC(const int n_cycles) {
         for (int i = 0; i < _config.n_particles; ++i) {
             // Move particle i slightly.
             for (int d = 0; d < _config.dims; ++d) {
-                R_new(i, d) = R_old(i, d) + _config.step_length * centered(rand_gen);
+                R_new(d, i) = R_old(d, i) + _config.step_length * centered(rand_gen);
             }
 
             update_distance_matrix(i, R_new);
@@ -211,14 +211,14 @@ Results VMCSolver::run_MC(const int n_cycles) {
                 // Update old <- new.
                 Psi_old = Psi_new;
                 for (int d = 0; d < _config.dims; ++d) {
-                    R_old(i, d) = R_new(i, d);
+                    R_old(d, i) = R_new(d, i);
                 }
             }
             else {
                 // Restore new <- old.
                 update_distance_matrix(i, R_old);
                 for (int d = 0; d < _config.dims; ++d) {
-                    R_new(i, d) = R_old(i, d);
+                    R_new(d, i) = R_old(d, i);
                 }
             }
 
