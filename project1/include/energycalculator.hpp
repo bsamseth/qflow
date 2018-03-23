6d24fb3e74d1f143df7195bee1b15c4f66a4edf9
Bendik Samseth <b.samseth@gmail.com>
2018-03-23 20:05:19 +0100

    Implement EnergyCalculator for dumping E_L to file.

---
+++ project1/include/energycalculator.hpp
@@ -0,0 +1,21 @@
+#include <string>
+
+#include "system.hpp"
+#include "wavefunction.hpp"
+#include "hamiltonian.hpp"
+#include "sampler.hpp"
+
+class EnergyCalculator : public Calculator {
+
+    protected:
+
+        std::ofstream _energy_file;
+        bool _analytic;
+
+        virtual void process_state(const System&);
+        virtual void finalize_calculation();
+
+    public:
+
+        EnergyCalculator(const Wavefunction&, const Hamiltonian&, Sampler&, std::string, bool);
+};
