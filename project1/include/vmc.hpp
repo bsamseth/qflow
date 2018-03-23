ded0ed54142d21baaea4ffa9e7d1eba3fc9dcebc
Bendik Samseth <b.samseth@gmail.com>
2018-03-23 20:06:24 +0100

    Add header file containing all project files.
    Just for convenience in main files.

---
+++ project1/include/vmc.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "boson.hpp"
+#include "config.hpp.in"
+#include "definitions.hpp"
+#include "hamiltonian.hpp"
+#include "harmonicoscillatorhamiltonian.hpp"
+#include "importancesampler.hpp"
+#include "interactinghamiltonian.hpp"
+#include "interactingwavefunction.hpp"
+#include "metropolissampler.hpp"
+#include "optimizer.hpp"
+#include "sampler.hpp"
+#include "simplegaussian.hpp"
+#include "system.hpp"
+#include "wavefunction.hpp"
+#include "calculator.hpp"
+#include "energycalculator.hpp"
