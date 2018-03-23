7db6710f49b63f499f3d8092615efee0ec725eb4
Bendik Samseth <b.samseth@gmail.com>
2018-03-23 20:05:49 +0100

    Add initial interface to a calculator.

--- project1/app/main.cpp
+++ project1/app/main.cpp
@@ -1,31 +1,59 @@
+#include <cstdlib>
 #include <iostream>
-#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <chrono>
 
-#include "config.hpp"
-#include "boson.hpp"
-#include "system.hpp"
-#include "wavefunction.hpp"
+#include "vmc.hpp"
 
-using namespace std;
+#define intarg(i) (atoi(argv[i]))
+#define floatarg(i) (atof(argv[i]))
 
-/*
- * Simple main program that demontrates how access
- * CMake definitions (here the version number) from source code.
- */
-int main() {
-    cout << "VMC v" << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << endl;
+using std::cout;
+using std::endl;
 
-    Boson b(3);
-    b[0] = 1;
-    b[2] = 3;
+int main(int argc, char *argv[]) {
 
-    cout << b << endl;
+    if (argc - 1 < 1) {
+        printf("Usage: ./main.x analytic[OFF=0,ON=1] sampler[Metro=0,Imp=1] dim n_bosons n_cycles alpha beta gamma a step filename \n");
+        return 0;
+    }
 
-    System s(10, 3);
+    bool analytic = intarg(1) != 0;
+    int sampler_type = intarg(2);
+    int dimensions = intarg(3);
+    int bosons = intarg(4);
+    long cycles = intarg(5);
+    Real alpha = floatarg(6);
+    Real beta = floatarg(7);
+    Real gamma = floatarg(8);
+    Real a = floatarg(9);
+    Real step = floatarg(10);
+    std::string filename = argv[11];
 
-    s[3] = b;
+    System *system = new System(bosons, dimensions);
+    Wavefunction *psi;
+    Hamiltonian *H;
+    Sampler *sampler;
 
-    cout << s << endl;
+    if (a != 0) {
+        psi = new InteractingWavefunction(alpha, beta, a);
+        H = new InteractingHamiltonian(gamma, a);
+    } else {
+        psi = new SimpleGaussian(alpha, beta);
+        H = new HarmonicOscillatorHamiltonian(gamma);
+    }
+
+    if (sampler_type == 0) {
+        sampler = new MetropolisSampler(*system, *psi, step);
+    } else {
+        sampler = new ImportanceSampler(*system, *psi, step);
+    }
+
+    Calculator *calc = new EnergyCalculator(*psi, *H, *sampler, filename, analytic);
+
+    calc->calculate(cycles);
 
     return 0;
 }
