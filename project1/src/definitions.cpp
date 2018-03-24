0b2d9cfa65d432f1fb0135378ac3683d3af556d3
Bendik Samseth <b.samseth@gmail.com>
2018-03-24 23:37:52 +0100

    Seed random numbers properly for stability.

--- project1/src/definitions.cpp
+++ project1/src/definitions.cpp
@@ -2,7 +2,7 @@
 #include "definitions.hpp"
 
 
-std::mt19937_64 rand_gen;
+std::mt19937_64 rand_gen (12345);
 std::uniform_real_distribution<Real> unif(0, 1);
 std::uniform_real_distribution<Real> centered(-0.5, 0.5);
 std::normal_distribution<Real> rnorm(0, 1);
