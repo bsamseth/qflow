24bb692e21f1c377dc98e9544ec58a86c155b7e5
Bendik Samseth <b.samseth@gmail.com>
2018-03-25 01:40:59 +0100

    Add Gross-Pitaevskii for testing elliptical exact.

--- project1/include/hamiltonian.hpp
+++ project1/include/hamiltonian.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cassert>
 
 #include "definitions.hpp"
 #include "wavefunction.hpp"
@@ -19,9 +20,19 @@ class Hamiltonian {
         virtual Real local_energy_numeric(const System&, const Wavefunction&) const;
         virtual Real kinetic_energy(System &, const Wavefunction&) const;
         virtual Real derivative_alpha(const System&, const Wavefunction&) const = 0;
+        Real gross_pitaevskii_energy(const System&, const Wavefunction&) const;
 
         friend std::ostream& operator<<(std::ostream&, const Hamiltonian&);
 };
+
+inline Real Hamiltonian::gross_pitaevskii_energy(const System &system, const Wavefunction &psi) const {
+    const int N = system.get_n_bosons();
+    const int D = system.get_dimensions();
+    const Real alpha = psi.get_alpha();
+    const Real beta = psi.get_beta();
+    assert(beta == _omega_z);   // GP equation used is for when gamma = beta = omega_z.
+    return N * (1 / (4 * alpha) + alpha) * (D == 3 ? 2 + beta : D) * 0.5;
+}
 
 inline std::ostream& operator<<(std::ostream &strm, const Hamiltonian &h) {
     return strm << "Hamiltonian(omega_z=" << h._omega_z
