5a35a32d30f1686437854c6d2132624fb2e928c2
Bendik Samseth <b.samseth@gmail.com>
2018-03-26 18:09:56 +0200

    Add thermalization to main script.
    Noticed some very bad energies in the beginning for
    the interacting case. Allow some extra cycles to make sure
    we only count from when the system is somewhat OK.

--- project1/app/main.cpp
+++ project1/app/main.cpp
@@ -21,6 +21,12 @@ int main(int argc, char *argv[]) {
     parse_arguments(argc, argv, &psi, &H, &sampler, &calc, &cycles);
 
     auto start_time = std::chrono::high_resolution_clock::now();
+
+    // Thermalize the sampler with 10% extra cycles.
+    for (int run = 0; run < cycles * 0.1; ++run) {
+        sampler->next_configuration();
+    }
+
     calc->calculate(cycles);
     auto end_time = std::chrono::high_resolution_clock::now();
     long long micro_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
