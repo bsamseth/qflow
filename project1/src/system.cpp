e5bc0ddd533bcf6f244c92a5d195ec283dda579b
Bendik Samseth <b.samseth@gmail.com>
2018-04-10 12:08:49 +0200

    Optimize distance update code.
    Small but measurable gains.

--- project1/src/system.cpp
+++ project1/src/system.cpp
@@ -13,14 +13,18 @@ Real System::distance(int i, int j) {
     assert(0 <= i and i < get_n_bosons());
     assert(0 <= j and j < get_n_bosons());
     if (_dirty[i]) {
-        for (int j = 0; j < get_n_bosons(); ++j) {
-            _distances[i][j] = _distances[j][i] = std::sqrt(square(_bosons[i] - _bosons[j]));
+        auto n = get_n_bosons();
+        const auto &bi = _bosons[i];
+        for (int k = 0; k < n; ++k) {
+            _distances[i][k] = _distances[k][i] = std::sqrt(square(bi - _bosons[k]));
         }
         _dirty[i] = false;
     }
     if (_dirty[j]) {
-        for (int i = 0; i < get_n_bosons(); ++i) {
-            _distances[i][j] = _distances[j][i] = std::sqrt(square(_bosons[i] - _bosons[j]));
+        auto n = get_n_bosons();
+        const auto &bj = _bosons[j];
+        for (int k = 0; k < n; ++k) {
+            _distances[k][j] = _distances[j][k] = std::sqrt(square(bj - _bosons[k]));
         }
         _dirty[j] = false;
     }
