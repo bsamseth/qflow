4c3c5dc6e160c60fe0576cceb73387e5d77c184e
Bendik Samseth <b.samseth@gmail.com>
2018-04-19 21:17:58 +0200

    Add main script for rbm.

---
+++ projects/app/rbm_main.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <chrono>
+
+#include "rbm.hpp"
+
+using std::cout;
+using std::endl;
+
+int main() {
+
+    System init_system (1, 1);
+    RBMWavefunction rbm (1, 2);
+    MetropolisSampler sampler (init_system, rbm, 0.5);
+    RBMHarmonicOscillatorHamiltonian H;
+
+    rbm.train(H, sampler, 10000, 1000000, 0.00001);
+
+}
