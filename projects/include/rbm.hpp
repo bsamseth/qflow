aa47e29edba9dd682b4e97869bd0a3a872b0c5b0
Bendik Samseth <b.samseth@gmail.com>
2018-04-19 21:16:41 +0200

    Make convenience include for RBM project.

---
+++ projects/include/rbm.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+/* Convenience include for all headers. */
+
+// Many cross dependencies to vmc project, so for simplicity:
+#include "vmc.hpp"
+
+#include "rbmwavefunction.hpp"
+#include "rbmharmonicoscillatorhamiltonian.hpp"
