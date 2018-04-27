5d55030eab1cdaa9bb6d7acceaab520143bf8cc1
Bendik Samseth <b.samseth@gmail.com>
2018-04-27 14:47:42 +0200

    Move E_L implementation to wavefunction.

--- projects/src/interactingwavefunction.cpp
+++ projects/src/interactingwavefunction.cpp
@@ -40,3 +40,53 @@ Real InteractingWavefunction::operator() (System &system) const {
     return correlation(system) * SimpleGaussian::operator()(system);
 }
 
+Real InteractingWavefunction::laplacian(System &system) const {
+    const auto alpha = _parameters[0];
+    const auto beta  = _parameters[1];
+    const auto a     = _parameters[2];
+    const Real one_body_beta_term = - (system.get_dimensions() == 3 ? 2 + beta : system.get_dimensions());
+
+    Real lap = 0;
+
+    for (int k = 0; k < system.get_n_particles(); ++k) {
+
+        const Vector &r_k = system[k];
+        Vector r_k_hat = system[k];
+        if (system.get_dimensions() == 3) {
+            r_k_hat[2] *= beta;
+        }
+
+        lap += 2 * alpha * (2 * alpha * square(r_k_hat) + one_body_beta_term);
+
+        // Interaction terms:
+
+        Vector term1 (system.get_dimensions());
+        Real term2 = 0;
+        Real term3 = 0;
+        for (int j = 0; j < system.get_n_particles(); ++j) {
+            if (j == k) continue;
+            const Vector r_kj = r_k - system[j];
+            const Real r_kj_norm = system.distance(k, j);
+
+            term1 += r_kj * (a / (square(r_kj_norm) * (r_kj_norm - a)));
+
+            term3 += a * (a - 2*r_kj_norm) / (square(r_kj_norm) * square(r_kj_norm - a))
+                + 2 * a / (square(r_kj_norm) * (r_kj_norm - a));
+
+            for (int i = 0; i < system.get_n_particles(); ++i) {
+                if (i == k) continue;
+                const Vector r_ki = r_k - system[i];
+                const Real r_ki_norm = system.distance(k, i);
+
+                term2 += (r_ki * r_kj) * square(a) / (square(r_ki_norm * r_kj_norm) * (r_ki_norm - a) * (r_kj_norm - a));
+            }
+        }
+
+        Real term1_final = -4 * alpha * (r_k_hat * term1);
+
+        lap += term1_final + term2 + term3;
+
+    }
+    return lap;
+}
+
