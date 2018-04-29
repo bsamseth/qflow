ac52c7f4635301a4f5a5aac90a236512d655a12a
Bendik Samseth <b.samseth@gmail.com>
2018-04-29 15:39:17 +0200

    Move RBM over to new wavefunction design.

--- projects/src/rbmwavefunction.cpp
+++ projects/src/rbmwavefunction.cpp
@@ -1,46 +1,39 @@
 #include <vector>
+#include <algorithm>
 #include <cassert>
 
 #include "prettyprint.hpp"
 #include "rbmwavefunction.hpp"
 
 
-
 RBMWavefunction::RBMWavefunction(int M, int N, Real sigma2)
     : _sigma2(sigma2),
       _M(M),
-      _N(N),
-      _a(M),
-      _b(N),
-      _w(M, std::vector<Real>(N))
+      _N(N)
 {
-    std::generate(_a.begin(), _a.end(), rnorm_small_func);
-    std::generate(_b.begin(), _b.end(), rnorm_small_func);
-    for (auto &row : _w) {
-        std::generate(row.begin(), row.end(), rnorm_small_func);
-    }
+    _parameters = Vector(M + N + M*N, rnorm_small_func);
 }
 
 Real RBMWavefunction::u_i(int i, System &system) const{
-    return square(system.degree(i) - _a[i]) / (2 * _sigma2);
+    return square(system.degree(i) - _parameters[a(i)]) / (2 * _sigma2);
 }
 
 Real RBMWavefunction::v_j(int j, System &system) const {
     Real v = 0;
     for (int i = 0; i < _M; ++i) {
-        v += system.degree(i) * _w[i][j];
+        v += system.degree(i) * _parameters[w(i, j)];
     }
-    return _b[j] + v / _sigma2;
+    return _parameters[b(j)] + v / _sigma2;
 }
 
 Real RBMWavefunction::operator() (System &system) const {
 
+    // Ensure that system is compatible with rbm, i.e. P*D = M.
     assert(system.get_dimensions() * system.get_n_particles() == _M);
 
     Real u = 0;
     for (int i = 0; i < _M; ++i) {
-        auto x_i = system.degree(i);
-        u += square( x_i - _a[i] );
+        u += square( system.degree(i) - _parameters[a(i)] );
     }
     Real visible = std::exp(- 0.5 * u / _sigma2);
 
@@ -53,7 +46,7 @@ Real RBMWavefunction::operator() (System &system) const {
 }
 
 Real RBMWavefunction::deriv_a(int k, System &system) const {
-    return (system.degree(k) - _a[k]) / _sigma2;
+    return (system.degree(k) - _parameters[a(k)]) / _sigma2;
 }
 
 Real RBMWavefunction::deriv_b(int k, System &system) const {
@@ -76,12 +69,12 @@ Real RBMWavefunction::laplacian(System &system) const {
 
         Real u = 0, v = 0;
         for (int j = 0; j < _N; ++j) {
-            u += square(_w[k][j]) * exp_v[j] / (1 + exp_v[j]);
-            v += _w[k][j] / (1 + exp_v[j]);
+            u += square(_parameters[w(k, j)]) * exp_v[j] / (1 + exp_v[j]);
+            v += _parameters[w(k, j)] / (1 + exp_v[j]);
         }
         u /= square(_sigma2);
 
-        v = square(_a[k] -system.degree(k) + v) / square(_sigma2);
+        v = square(_parameters[a(k)] - system.degree(k) + v) / square(_sigma2);
 
         res += -1 / _sigma2 - u + v;
     }
@@ -136,41 +129,20 @@ void RBMWavefunction::train(const Hamiltonian &hamiltonian,
             Real E = hamiltonian.local_energy(system, *this);
             E_mean += E;
 
-
-            int k = 0;
-            for (int i = 0; i < _M; ++i, ++k) {
-                auto d = deriv_a(i, system);
-                grad[k] += d;
-                grad_E[k] += E * d;
-            }
-            for (int j = 0; j < _N; ++j, ++k) {
-                auto d = deriv_b(j, system);
-                grad[k] += d;
-                grad_E[k] += E * d;
-            }
-            for (int i = 0; i < _M; ++i) {
-                for (int j = 0; j < _N; ++j, ++k) {
-                    auto d = deriv_w(i, j, system);
-                    grad[k] += d;
-                    grad_E[k] += E * d;
-                }
-            }
-
-            assert(k == grad.size());
+            Vector g = gradient(system);
+            grad += g;
+            grad_E += g * E;
         }
 
         E_mean /= sample_points;
-        grad *= 1/sample_points;
-        grad_E *= 1/sample_points;
-        _parameters -= learning_rate * 2 * (grad_E - E_mean * grad);
+        grad /= sample_points;
+        grad_E /= sample_points;
+        updates = -learning_rate * 2 * (grad_E - E_mean * grad);
+        _parameters += updates;
 
         printf("Iteration %d: <E> = %g\n", iteration, E_mean);
         std::cout << "updates: " << updates << '\n';
-        std::cout << "a = " << _a << "\n";
-        std::cout << "b = " << _b << "\n";
-        std::cout << "w = " << _w << "\n";
-
-
+        std::cout << "params = " << _parameters << '\n';
     }
 }
 
--- projects/tests/rbmwavefunction.cpp
+++ projects/tests/rbmwavefunction.cpp
@@ -22,21 +22,38 @@ namespace {
 class RBMWavefunctionTest : public ::testing::Test {
     protected:
         constexpr static int P = 2;
-        constexpr static int D = 1;
+        constexpr static int D = 3;
         constexpr static int M = P * D;
         constexpr static int N = M + 1;
 
         System *s;
         RBMWavefunction *rbm;
-        const std::vector<Real> params = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
         virtual void SetUp() {
-            rbm = new RBMWavefunction(M, N, 2);
-            rbm->set_parameters(params);
             s = new System(P, D);
-            (*s)(0)[0] = 1.5;
-            (*s)(1)[0] = 2.5;
+            (*s)(0) = {{-0.5009136425144746,  0.0118290278182348,  1.0861838035029527}};
+            (*s)(1) = {{-0.1180658328483096, -1.0091187557423571, -0.5365339555565911}};
 
+            rbm = new RBMWavefunction(M, N, 2);
+            rbm->set_parameters({{-0.7665546243072233,  0.9406054576757206, -0.1411666581066846,
+                                   2.3698247761289206, -0.0925344556191133, -0.822443320890463 ,
+                                  -0.3367728130900145,  0.6042382684274119,  1.1031676900053826,
+                                  -1.11343573670209  , -1.2698025674851516,  0.6405021942562454,
+                                   1.1083481552380658,  0.9132139881362205, -0.5289324083374893,
+                                  -0.6259702462677719,  0.4288389687300546, -0.2275709099552632,
+                                   0.0800292163048842, -0.2006399070516838,  2.1892037314628694,
+                                  -0.1135711243410274, -0.7075746186364059, -0.9951770429084715,
+                                   0.0992837432654784, -1.1374021103825422, -0.2257842764845613,
+                                   1.0681137462003307,  0.4475426303688199,  1.2644501323447888,
+                                   0.8874277315102514,  0.9465699704550599, -2.0148512749309386,
+                                  -0.4172389063717908,  0.8106036574891917,  1.0111662774773056,
+                                  -0.2573547886564483,  0.3167544620067116,  0.0489496271535557,
+                                  -0.177898745022331 ,  0.4915684273366239, -0.7627213279660949,
+                                   0.4743876191911438,  0.8256348759803365,  0.3608972886871439,
+                                   1.0230129736402584,  0.4995386943252447, -0.9389995079735179,
+                                  -0.1790977108647215, -0.0797636435834231, -0.6916788192310428,
+                                  -0.0363434391489085, -1.2944879136187115,  0.8263967557636448,
+                                  -0.0240516420501065}});
         }
         virtual void TearDown() {
             delete s;
@@ -45,52 +62,67 @@ class RBMWavefunctionTest : public ::testing::Test {
 };
 
 TEST_F(RBMWavefunctionTest, init) {
-    int k = 0;
-    for (auto a_i : rbm->get_visible_bias()) {
-        ASSERT_EQ(params[k++], a_i);
-    }
-    for (auto b_i : rbm->get_hidden_bias()) {
-        ASSERT_EQ(params[k++], b_i);
-    }
-    for (auto w_i : rbm->get_weights()) {
-        for (auto w_ij : w_i) {
-            ASSERT_EQ(params[k++], w_ij);
-        }
-    }
+    ASSERT_EQ(M + N + M*N, rbm->get_parameters().size());
+    ASSERT_DOUBLE_EQ(-0.7665546243072233, rbm->get_parameters()[rbm->a(0)]);
+    ASSERT_DOUBLE_EQ(-0.822443320890463, rbm->get_parameters()[rbm->a(M-1)]);
+    ASSERT_DOUBLE_EQ(-0.3367728130900145, rbm->get_parameters()[rbm->b(0)]);
+    ASSERT_DOUBLE_EQ(1.1083481552380658, rbm->get_parameters()[rbm->b(N-1)]);
+    ASSERT_DOUBLE_EQ(0.9132139881362205, rbm->get_parameters()[rbm->w(0, 0)]);
+    ASSERT_DOUBLE_EQ(-0.0240516420501065, rbm->get_parameters()[rbm->w(M-1, N-1)]);
 }
 
 TEST_F(RBMWavefunctionTest, evaluation) {
     // Values calculated by hand/calculator.
-    EXPECT_DOUBLE_EQ(1.920561139962089e28, rbm->operator()(*s));
+    EXPECT_DOUBLE_EQ(62.977993642663755, rbm->operator()(*s));
 }
 
 TEST_F(RBMWavefunctionTest, gradient) {
     Vector gradient = rbm->gradient(*s);
-    EXPECT_DOUBLE_EQ(0.25, gradient[0]);
-    EXPECT_DOUBLE_EQ(0.25, gradient[1]);
+    Vector expected_a = {{0.1328204908963744, -0.4643882149287429,  0.6136752308048187,
+                          -1.2439453044886151, -0.4582921500616219,  0.142954682666936}};
+    Vector expected_b = {{0.6017040564983215,  0.6685085554779254,  0.8489820123505009,
+                          0.2817462931894426,  0.2952675674909326,  0.2801418147825886,
+                          0.7992927449865912}};
+    Vector expected_w = {{-0.1507008853281547045 , -0.16743252778826867022,
+                          -0.21263333611787901822, -0.07056528099323740311,
+                          -0.07395177637413576088, -0.07016342843168088539,
+                          -0.20018832016331319923,  0.00355878701133167767,
+                           0.00395390314973816364,  0.00502131592063750822,
+                           0.00166639236991122409,  0.00174636413483637842,
+                           0.00165690266005700778,  0.00472742805767981198,
+                           0.32678060033525119232,  0.36306158273163885442,
+                           0.46107525564022888709,  0.153014130179683433  ,
+                           0.16035742475418299224,  0.15214275095038592989,
+                           0.43408941693092562009, -0.03552034527934033042,
+                          -0.03946400968436082624, -0.0501178841806978817 ,
+                          -0.01663230537866778116, -0.01743050563445571605,
+                          -0.0165375883389716101 , -0.04718458181322671835,
+                          -0.30359542440935755048, -0.33730226085350228926,
+                          -0.4283618359753899596 , -0.14215773440917583992,
+                          -0.14898002015876118942, -0.14134817978240585279,
+                          -0.40329065014738102457, -0.16141732875374550926,
+                          -0.17933876979699708842, -0.22775383864140447132,
+                          -0.07558322657416934043, -0.07921053796674140668,
+                          -0.07515279800105209573, -0.214423849057670729}};
+    for (int i = 0; i < M; ++i)
+        EXPECT_DOUBLE_EQ(expected_a[i], gradient[rbm->a(i)]);
+    for (int j = 0; j < N; ++j)
+        EXPECT_DOUBLE_EQ(expected_b[j], gradient[rbm->b(j)]);
+    // d/dw gives some very minor differences from expected values.
+    // This is so small that it is with all likelihood rounding errors
+    // in both calculated values.
+    for (int i = 0; i < M; ++i)
+        for (int j = 0; j < N; ++j)
+            EXPECT_NEAR(expected_w[i * N + j], gradient[rbm->w(i, j)], 1e-16);
+}
 
-    EXPECT_DOUBLE_EQ(0.9999999928058669, gradient[2]);
-    EXPECT_DOUBLE_EQ(0.9999999996418252, gradient[3]);
-    EXPECT_DOUBLE_EQ(0.9999999999821676, gradient[4]);
-
-    EXPECT_DOUBLE_EQ(0.7499999946044003, gradient[5]);
-    EXPECT_DOUBLE_EQ(0.7499999997313689, gradient[6]);
-    EXPECT_DOUBLE_EQ(0.7499999999866257, gradient[7]);
-    EXPECT_DOUBLE_EQ(1.2499999910073336, gradient[8]);
-    EXPECT_DOUBLE_EQ(1.2499999995522815, gradient[9]);
-    EXPECT_DOUBLE_EQ(1.2499999999777094, gradient[10]);
-
-    // This is not perfectly reproducing the calculator results,
-    // but after reviewing code thoroughly, no fault is found,
-    // and the discrepancy is deemed likely to be the result of rounding
-    // errors, in one or both of the actual vs. expected numbers. Single
-    // precision still satisfied.
-    EXPECT_FLOAT_EQ(321.62499854823926, rbm->laplacian(*s));
+TEST_F(RBMWavefunctionTest, laplacian) {
+    EXPECT_DOUBLE_EQ(-1.472115593929292, rbm->laplacian(*s));
 }
 
 TEST_F(RBMWavefunctionTest, RBMHarmonicOscillatorHamiltonian) {
     RBMHarmonicOscillatorHamiltonian H;
-    EXPECT_DOUBLE_EQ(4.25, H.external_potential(*s));
+    EXPECT_DOUBLE_EQ(1.3754892738453772, H.external_potential(*s));
     EXPECT_DOUBLE_EQ(0.0,  H.internal_potential(*s));
     EXPECT_DOUBLE_EQ(H.external_potential(*s) - 0.5 * rbm->laplacian(*s), H.local_energy(*s, *rbm));
 }
