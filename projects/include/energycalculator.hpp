3bcb15b8d74cdf4bf5027bfb1170e082db5e9a98
Bendik Samseth <b.samseth@gmail.com>
2018-05-15 20:25:49 +0200

    Add missing includes.
    Once again, this worked only due to fstream being imported
    elsewhere, but when imported by it self, this fails. Now it should
    be ok.

--- projects/include/energycalculator.hpp
+++ projects/include/energycalculator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <fstream>
 
 #include "system.hpp"
 #include "wavefunction.hpp"
--- projects/include/onebodydensitycalculator.hpp
+++ projects/include/onebodydensitycalculator.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
 
 #include "system.hpp"
 #include "wavefunction.hpp"
