0e780fcbf3691575fe079de5102565b16e2f428e
Bendik Samseth <b.samseth@gmail.com>
2018-05-15 20:34:31 +0200

    Try adam in rbm main.

--- projects/app/rbm_main.cpp
+++ projects/app/rbm_main.cpp
@@ -12,11 +12,21 @@ using std::endl;
 
 int main() {
 
-    System init_system (1, 1);
-    RBMWavefunction rbm (1, 2, 1);
-    MetropolisSampler sampler (init_system, rbm, 0.5);
-    RBMHarmonicOscillatorHamiltonian H;
+    System init_system (2, 2);
+    RBMWavefunction rbm (4, 10, 1);
+    ImportanceSampler sampler (init_system, rbm, 0.5);
+    RBMInteractingHamiltonian H;
 
-    rbm.train(H, sampler, 10000, 1000, 0.5, 0.1);
 
+    std::cout << rbm.get_parameters() << std::endl;
+
+    AdamOptimizer optimizer(rbm.get_parameters().size());
+    rbm.train(H, sampler, 1000, 1e3, optimizer, 0.0, true);
+    printf("AR = %g\n", sampler.get_acceptance_rate());
+    std::cout << rbm.get_parameters() << std::endl;
+
+    Real E = 0;
+    for (long i = 0; i < (long) 1e6; ++i)
+        E += H.local_energy(sampler.next_configuration(), rbm);
+    std::cout << "<E> = " << E / 1e6 << std::endl;
 }
