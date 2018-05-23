6721d5eea7eaab492e2e50716a1368308b70d03e
Bendik Samseth <b.samseth@gmail.com>
2018-05-23 22:27:12 +0200

    Parametrize rbm main program.
    Not really using this, thought I might for a while,
    but think I'll stick to just using the python bindings.

--- projects/app/rbm_main.cpp
+++ projects/app/rbm_main.cpp
@@ -10,23 +10,38 @@
 using std::cout;
 using std::endl;
 
-int main() {
+int main(int argc, char **argv) {
+
+    if (argc < 6) {
+        printf("Usage: %s N iterations samples omega gamma\n", argv[0]);
+        return 0;
+    }
+    const int N = atoi(argv[1]);
+    const long iterations = atoi(argv[2]);
+    const long samples = atoi(argv[3]);
+    const Real omega = atof(argv[4]);
+    const Real gamma = atof(argv[5]);
+
+    rand_gen.seed(1234);
 
     System init_system (2, 2);
-    RBMWavefunction rbm (4, 10, 1);
-    ImportanceSampler sampler (init_system, rbm, 0.5);
-    RBMInteractingHamiltonian H;
-
+    RBMWavefunction rbm (4, N, 1);
+    ImportanceSampler sampler (init_system, rbm, 0.1);
+    RBMInteractingHamiltonian H(omega);
 
     std::cout << rbm.get_parameters() << std::endl;
-
-    AdamOptimizer optimizer(rbm.get_parameters().size());
-    rbm.train(H, sampler, 1000, 1e3, optimizer, 0.0, true);
-    printf("AR = %g\n", sampler.get_acceptance_rate());
-    std::cout << rbm.get_parameters() << std::endl;
-
     Real E = 0;
     for (long i = 0; i < (long) 1e6; ++i)
         E += H.local_energy(sampler.next_configuration(), rbm);
-    std::cout << "<E> = " << E / 1e6 << std::endl;
+    std::cout << "Before training: <E> = " << E / 1e6 << std::endl;
+
+    AdamOptimizer optimizer(rbm.get_parameters().size());
+    rbm.train(H, sampler, iterations, samples, optimizer, gamma, true);
+    printf("AR = %g\n", sampler.get_acceptance_rate());
+    std::cout << rbm.get_parameters() << std::endl;
+
+    E = 0;
+    for (long i = 0; i < (long) 1e6; ++i)
+        E += H.local_energy(sampler.next_configuration(), rbm);
+    std::cout << "After training: <E> = " << E / 1e6 << std::endl;
 }
