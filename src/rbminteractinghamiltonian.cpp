0d6b529737f933db2f54c32f002b83c9b02a10e2
Bendik Samseth <b.samseth@gmail.com>
2019-01-23 12:42:48 +0100

    Bugfix: Fix column/row confusion in RBMInteractingHamiltonian.
    Likely a copy error when porting to Eigen, or when changing from column
    major to row major storage order.

--- src/rbminteractinghamiltonian.cpp
+++ src/rbminteractinghamiltonian.cpp
@@ -4,7 +4,7 @@
 RBMInteractingHamiltonian::RBMInteractingHamiltonian(Real omega) : RBMHarmonicOscillatorHamiltonian(omega) {}
 
 Real RBMInteractingHamiltonian::internal_potential(System &system) const {
-    const auto P = system.cols();
+    const auto P = system.rows();
     Real pot = 0;
     for (int i = 0; i < P - 1; ++i) {
         for (int j = i + 1; j < P; ++j) {
