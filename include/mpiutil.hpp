dfc90aa6dcc11bb9b3ecc637d3d5aaedd092c8ef
Bendik Samseth <b.samseth@gmail.com>
2019-02-17 13:39:40 +0100

    Add utility to fetch number of MPI procs

--- include/mpiutil.hpp
+++ include/mpiutil.hpp
@@ -6,4 +6,10 @@ namespace mpiutil {
 void initialize_mpi(void);
 void library_onexit(void);
 
+inline int proc_count(void) {
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    return world_size;
+}
+
 }
