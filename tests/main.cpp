3e1bfeb77d6dd6e3d3b5e807397ecd49f384bee0
Bendik Samseth <b.samseth@gmail.com>
2019-02-17 14:29:58 +0100

    Setup MPI in C++ tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,12 @@
 // Bring in gtest
 #include <gtest/gtest.h>
 #include <cstdlib>
+#include "mpiutil.hpp"
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
     std::srand(2018);
+    mpiutil::initialize_mpi();
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
