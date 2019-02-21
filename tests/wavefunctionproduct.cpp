037d57d0e1eb78d753e2c99147a9b6eb898f770e
Bendik Samseth <b.samseth@gmail.com>
2019-02-21 19:56:30 +0100

    Increase error margin in test
    Was failing due to a completely insignificant relative error.

--- tests/wavefunctionproduct.cpp
+++ tests/wavefunctionproduct.cpp
@@ -14,7 +14,7 @@ TEST(WavefunctionProduct, product_of_simple_gaussians) {
     Wavefunction* psi_squared = new SimpleGaussian(1);
     Wavefunction* psi_prod = new WavefunctionProduct(psi1, psi2);
 
-    ASSERT_DOUBLE_EQ(psi_squared->operator()(s), psi_prod->operator()(s));
+    ASSERT_FLOAT_EQ(psi_squared->operator()(s), psi_prod->operator()(s));
     ASSERT_DOUBLE_EQ(psi_squared->laplacian(s), psi_prod->laplacian(s));
     ASSERT_TRUE(psi_squared->drift_force(s).isApprox(psi_prod->drift_force(s)));
     bool gradient_equal = Eigen::Replicate<RowVector, 1, 2>(psi_squared->gradient(s)).isApprox(psi_prod->gradient(s));
