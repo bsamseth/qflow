095286f4349a111c316929fc06adda14c24d1670
Bendik Samseth <b.samseth@gmail.com>
2019-02-21 19:49:24 +0100

    Use rowwise storage order for systems
    Thought this was done already, but now it is!

--- include/system.hpp
+++ include/system.hpp
@@ -4,7 +4,7 @@
 #include "definitions.hpp"
 #include "vector.hpp"
 
-using System = Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic>;
+using System = Matrix;
 
 inline Real distance(const System& system, int i, int j) {
     return norm(system.row(i) - system.row(j));
