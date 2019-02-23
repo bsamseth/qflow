82c19f388f186bb0735a61ef7b57f4b916e3b275
Bendik Samseth <b.samseth@gmail.com>
2019-02-23 12:13:06 +0100

    Add training plot helper function to qflow

--- qflow/__init__.py
+++ qflow/__init__.py
@@ -1,6 +1,13 @@
 from _qflow_backend import *
 from _qflow_backend import _init_mpi
 
-__all__ = ["wavefunctions", "hamiltonians", "samplers", "optimizers", "statistics"]
+__all__ = [
+    "wavefunctions",
+    "hamiltonians",
+    "samplers",
+    "optimizers",
+    "statistics",
+    "training",
+]
 
 _init_mpi()
