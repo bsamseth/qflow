4b8d0a0efdab0aaf2ec9295f475bdfccc6cbb2bf
Bendik Samseth <b.samseth@gmail.com>
2019-03-03 11:40:55 +0100

    Pytest-ify tests

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -1,69 +1,65 @@
-import unittest
-
 import numpy as np
 
 from qflow.layers.activations import exponential, identity, relu, sigmoid, tanh
 
 
-class TestDnn(unittest.TestCase):
-    def setUp(self):
-        pass
+def test_identity():
+    for rows, cols in np.random.randint(1, 100, size=(500, 2)):
+        x = np.random.randn(rows, cols)
 
-    def test_identity(self):
-        for rows, cols in np.random.randint(1, 100, size=(500, 2)):
-            x = np.random.randn(rows, cols)
+        np.testing.assert_array_equal(x, identity.evaluate(x))
+        np.testing.assert_array_equal(np.ones_like(x), identity.derivative(x))
+        np.testing.assert_array_equal(np.zeros_like(x), identity.dbl_derivative(x))
 
-            np.testing.assert_array_equal(x, identity.evaluate(x))
-            np.testing.assert_array_equal(np.ones_like(x), identity.derivative(x))
-            np.testing.assert_array_equal(np.zeros_like(x), identity.dbl_derivative(x))
 
-    def test_relu(self):
-        for rows, cols in np.random.randint(1, 100, size=(500, 2)):
-            x = np.random.randn(rows, cols)
+def test_relu():
+    for rows, cols in np.random.randint(1, 100, size=(500, 2)):
+        x = np.random.randn(rows, cols)
 
-            np.testing.assert_array_equal(np.where(x > 0, x, 0), relu.evaluate(x))
-            np.testing.assert_array_equal(
-                np.where(x > 0, 1, 0), relu.derivative(relu.evaluate(x))
-            )
-            np.testing.assert_array_equal(
-                np.zeros_like(x), relu.dbl_derivative(relu.evaluate(x))
-            )
+        np.testing.assert_array_equal(np.where(x > 0, x, 0), relu.evaluate(x))
+        np.testing.assert_array_equal(
+            np.where(x > 0, 1, 0), relu.derivative(relu.evaluate(x))
+        )
+        np.testing.assert_array_equal(
+            np.zeros_like(x), relu.dbl_derivative(relu.evaluate(x))
+        )
 
-    def test_sigmoid(self):
-        for rows, cols in np.random.randint(1, 100, size=(500, 2)):
-            x = np.random.randn(rows, cols)
-            sig = 1 / (1 + np.exp(-x))
 
-            np.testing.assert_array_equal(sig, sigmoid.evaluate(x))
-            np.testing.assert_array_equal(sig * (1 - sig), sigmoid.derivative(sig))
-            np.testing.assert_array_equal(
-                sig * (1 - sig) * (1 - 2 * sig), sigmoid.dbl_derivative(sig)
-            )
+def test_sigmoid():
+    for rows, cols in np.random.randint(1, 100, size=(500, 2)):
+        x = np.random.randn(rows, cols)
+        sig = 1 / (1 + np.exp(-x))
 
-    def test_tanh(self):
-        for rows, cols in np.random.randint(1, 100, size=(50, 2)):
-            x = np.random.randn(rows, cols)
-            ta = np.tanh(x)
+        np.testing.assert_array_equal(sig, sigmoid.evaluate(x))
+        np.testing.assert_array_equal(sig * (1 - sig), sigmoid.derivative(sig))
+        np.testing.assert_array_equal(
+            sig * (1 - sig) * (1 - 2 * sig), sigmoid.dbl_derivative(sig)
+        )
 
-            np.testing.assert_array_equal(ta, tanh.evaluate(x))
-            np.testing.assert_array_equal(
-                1 - ta ** 2, tanh.derivative(tanh.evaluate(x))
-            )
-            np.testing.assert_allclose(
-                -2 * np.sinh(x) / np.cosh(x) ** 3,
-                tanh.dbl_derivative(tanh.evaluate(x)),
-                rtol=1e-12,
-            )
 
-    def test_exponential(self):
-        for rows, cols in np.random.randint(1, 100, size=(50, 2)):
-            x = np.random.randn(rows, cols)
-            exp = np.exp(x)
+def test_tanh():
+    for rows, cols in np.random.randint(1, 100, size=(50, 2)):
+        x = np.random.randn(rows, cols)
+        ta = np.tanh(x)
 
-            np.testing.assert_array_equal(exp, exponential.evaluate(x))
-            np.testing.assert_array_equal(
-                exp, exponential.derivative(exponential.evaluate(x))
-            )
-            np.testing.assert_array_equal(
-                exp, exponential.dbl_derivative(exponential.evaluate(x))
-            )
+        np.testing.assert_array_equal(ta, tanh.evaluate(x))
+        np.testing.assert_array_equal(1 - ta ** 2, tanh.derivative(tanh.evaluate(x)))
+        np.testing.assert_allclose(
+            -2 * np.sinh(x) / np.cosh(x) ** 3,
+            tanh.dbl_derivative(tanh.evaluate(x)),
+            rtol=1e-12,
+        )
+
+
+def test_exponential():
+    for rows, cols in np.random.randint(1, 100, size=(50, 2)):
+        x = np.random.randn(rows, cols)
+        exp = np.exp(x)
+
+        np.testing.assert_array_equal(exp, exponential.evaluate(x))
+        np.testing.assert_array_equal(
+            exp, exponential.derivative(exponential.evaluate(x))
+        )
+        np.testing.assert_array_equal(
+            exp, exponential.dbl_derivative(exponential.evaluate(x))
+        )
--- tests/test_fixedwavefunction.py
+++ tests/test_fixedwavefunction.py
@@ -1,44 +1,43 @@
-import unittest
-
 import numpy as np
 
 from qflow.wavefunctions import FixedWavefunction, SimpleGaussian, WavefunctionProduct
 
 
-class TestWavefunctionProduct(unittest.TestCase):
-    def test_consistent_with_original(self):
-        orig = SimpleGaussian()
-        fixed = FixedWavefunction(orig)
+def test_consistent_with_original():
+    orig = SimpleGaussian()
+    fixed = FixedWavefunction(orig)
 
-        for _ in range(100):
-            n, d = np.random.randint(100), np.random.randint(1, 3 + 1)
-            s = np.random.randn(n, d)
+    for _ in range(100):
+        n, d = np.random.randint(100), np.random.randint(1, 3 + 1)
+        s = np.random.randn(n, d)
 
-            self.assertAlmostEqual(orig(s), fixed(s))
-            self.assertAlmostEqual(orig.laplacian(s), fixed.laplacian(s))
-            np.testing.assert_allclose(orig.drift_force(s), fixed.drift_force(s))
-            np.testing.assert_array_equal(orig.parameters, fixed.parameters)
-
-            # Gradient is special, should return zeros for each parameter as they are fixed.
-            np.testing.assert_array_equal(orig.gradient(s) * 0, fixed.gradient(s))
-
-    def test_set_parameters_is_noop(self):
-        orig = SimpleGaussian()
-        fixed = FixedWavefunction(orig)
-
-        # This should do noting to the parameters.
-        fixed.parameters = orig.parameters + 1
-
+        assert np.isclose(orig(s), fixed(s))
+        assert np.isclose(orig.laplacian(s), fixed.laplacian(s))
+        np.testing.assert_allclose(orig.drift_force(s), fixed.drift_force(s))
         np.testing.assert_array_equal(orig.parameters, fixed.parameters)
 
-    def test_set_parameters_is_noop_in_wavefunction_product(self):
-        orig1 = SimpleGaussian(0.4)
-        fixed = FixedWavefunction(orig1)
-        orig2 = SimpleGaussian(0.6)
-        prod = WavefunctionProduct(fixed, orig2)
+        # Gradient is special, should return zeros for each parameter as they are fixed.
+        np.testing.assert_array_equal(orig.gradient(s) * 0, fixed.gradient(s))
 
-        # Only the params related to orig2 should change after this.
-        # Also, orig2's beta should not change because it is const by def.
-        prod.parameters = [0.5, 1.1, 123, 321]
 
-        np.testing.assert_array_equal(prod.parameters, [0.4, 1, 123, 1])
+def test_set_parameters_is_noop():
+    orig = SimpleGaussian()
+    fixed = FixedWavefunction(orig)
+
+    # This should do noting to the parameters.
+    fixed.parameters = orig.parameters + 1
+
+    np.testing.assert_array_equal(orig.parameters, fixed.parameters)
+
+
+def test_set_parameters_is_noop_in_wavefunction_product():
+    orig1 = SimpleGaussian(0.4)
+    fixed = FixedWavefunction(orig1)
+    orig2 = SimpleGaussian(0.6)
+    prod = WavefunctionProduct(fixed, orig2)
+
+    # Only the params related to orig2 should change after this.
+    # Also, orig2's beta should not change because it is const by def.
+    prod.parameters = [0.5, 1.1, 123, 321]
+
+    np.testing.assert_array_equal(prod.parameters, [0.4, 1, 123, 1])
