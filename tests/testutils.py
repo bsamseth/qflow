5e3b909b884a78f15c9c0d4abd6fd0bf47b20771
Bendik Samseth <b.samseth@gmail.com>
2019-03-05 14:21:10 +0100

    Update test utils

--- tests/testutils.py
+++ tests/testutils.py
@@ -2,23 +2,42 @@ import numpy as np
 from hypothesis import strategies as st
 from hypothesis.extra.numpy import array_shapes, arrays
 
-float_strat = lambda: st.floats(allow_nan=False, allow_infinity=False, width=32)
-array_strat = lambda min_size=1, max_size=10: arrays(
-    dtype=np.float64,
-    shape=array_shapes(min_dims=2, max_dims=2, min_side=min_size, max_side=max_size),
-    elements=float_strat(),
-)
+
+@st.composite
+def position_array_shapes(draw, min_dims=1, max_dims=3, max_size=50):
+    """Return a strategy for array shapes ([1...max_size], [1...3])"""
+    dims = draw(st.integers(min_dims, max_dims))
+    N = draw(st.integers(1, max_size))
+    return (N, dims)
+
+
+def float_strat():
+    return st.floats(allow_nan=False, allow_infinity=False, width=16)
+
+
+def array_strat(min_dims=1, max_dims=3, max_size=50):
+    return arrays(
+        dtype=np.float64,
+        shape=position_array_shapes(
+            min_dims=min_dims, max_dims=max_dims, max_size=max_size
+        ),
+        elements=float_strat(),
+    )
 
 
 def assert_close(expected, actuals, rtol=1e-05, atol=1e-08):
-    assert np.shape(expected) == np.shape(actuals)
+    assert np.shape(expected) == np.shape(
+        actuals
+    ), f"{np.shape(expected)} != {np.shape(actuals)}"
     expected, actuals = np.asarray(expected).ravel(), np.asarray(actuals).ravel()
 
     for expect, actual in zip(expected, actuals):
         if not np.isfinite(expect):
             continue
 
+        error_msg = f"Expected {expect}, actual {actual}"
+
         if expect == 0:
-            assert expect == actual
+            assert abs(actual) < atol, error_msg
         else:
-            assert np.isclose(1, actual / expect, rtol=rtol, atol=atol)
+            assert np.isclose(1, actual / expect, rtol=rtol, atol=atol), error_msg
