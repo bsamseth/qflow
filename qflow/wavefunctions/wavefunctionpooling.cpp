6493437c02ccb6af77fa5f80a61cd54ebd64ac93
Bendik Samseth <b.samseth@gmail.com>
2019-03-08 08:53:18 +0100

    Revert "omp parallelize sum pooling"
    This reverts commit e0e1c3dbd204853e9474daa8f4bb2b44f08f9cfd.
    
    The parallelization of SumPooling cannot be implemented at present,
    because some wavefunctions are not thread safe. This refers as of now
    only to Dnn, and is because each layer has a stored state that is
    changed on calls. Having multiple threads calling the evaluation leads
    to bad things (malloc errors at runtime).
    
    If this is to be reintroduced, it's likely that each thread would need a
    copy of the wavefunction. This may, however, mean significant copying
    work that might only be worth it for very large N. Not to mention the
    issues of making a copy of an abstract base class pointer...

--- qflow/wavefunctions/wavefunctionpooling.cpp
+++ qflow/wavefunctions/wavefunctionpooling.cpp
@@ -1,7 +1,5 @@
 #include "wavefunctionpooling.hpp"
 
-#include "omp.h"
-
 SumPooling::SumPooling(Wavefunction& psi) : f(&psi)
 {
     _parameters = f->get_parameters();
@@ -15,151 +13,95 @@ void SumPooling::set_parameters(const RowVector& a)
 
 Real SumPooling::operator()(const System& system)
 {
-    const int N  = system.rows();
-    Real      s1 = 0;
-    Real      s2 = 0;
-    System    sub1(2, system.cols());
-    System    sub2(2, system.cols());
+    const int N   = system.rows();
+    Real      sum = 0;
+    System    sub(2, system.cols());
     for (int i = 0; i < N; ++i)
     {
-        sub1.row(0) = system.row(i);
-        sub2.row(0) = system.row(i);
-#pragma omp parallel sections
+        sub.row(0) = system.row(i);
+        for (int j = 0; j < N; ++j)
         {
-#pragma omp section
+            if (i != j)
             {
-                for (int j = 0; j < i; ++j)
-                {
-                    sub1.row(1) = system.row(j);
-                    s1 += (*f)(sub1);
-                }
-            }
-#pragma omp section
-            {
-                for (int j = i + 1; j < N; ++j)
-                {
-                    sub2.row(1) = system.row(j);
-                    s2 += (*f)(sub2);
-                }
+                sub.row(1) = system.row(j);
+                sum += (*f)(sub);
             }
         }
     }
-    return s1 + s2;
+
+    return sum;
 }
 
 RowVector SumPooling::gradient(const System& system)
 {
-    const int N  = system.rows();
-    RowVector g1 = RowVector::Zero(f->get_parameters().size());
-    RowVector g2 = RowVector::Zero(f->get_parameters().size());
-    System    sub1(2, system.cols());
-    System    sub2(2, system.cols());
-    Real      d1 = 0;
-    Real      d2 = 0;
+    const int N    = system.rows();
+    RowVector grad = RowVector::Zero(f->get_parameters().size());
+    System    sub(2, system.cols());
+    Real      divisor = 0;
     for (int i = 0; i < N; ++i)
     {
-        sub1.row(0) = system.row(i);
-        sub2.row(0) = system.row(i);
-#pragma omp parallel sections
+        sub.row(0) = system.row(i);
+        for (int j = 0; j < N; ++j)
         {
-#pragma omp section
+            if (i != j)
             {
-                for (int j = 0; j < i; ++j)
-                {
-                    sub1.row(1) = system.row(j);
-                    Real eval   = (*f)(sub1);
-                    g1 += eval * f->gradient(sub1);
-                    d1 += eval;
-                }
-            }
-#pragma omp section
-            {
-                for (int j = i + 1; j < N; ++j)
-                {
-                    sub2.row(1) = system.row(j);
-                    Real eval   = (*f)(sub2);
-                    g2 += eval * f->gradient(sub2);
-                    d2 += eval;
-                }
+                sub.row(1) = system.row(j);
+                Real eval  = (*f)(sub);
+                grad += eval * f->gradient(sub);
+                divisor += eval;
             }
         }
     }
-    return (g1 + g2) / (d1 + d2);
+    return grad / divisor;
 }
 
 Real SumPooling::drift_force(const System& system, int k, int dim_index)
 {
-    const int N  = system.rows();
-    Real      d1 = 0;
-    Real      d2 = 0;
-    System    sub1(2, system.cols());
-    System    sub2(2, system.cols());
-#pragma omp parallel sections
+    const int N     = system.rows();
+    Real      drift = 0;
+    System    sub(2, system.cols());
+
+    sub.row(0) = system.row(k);
+    for (int j = 0; j < N; ++j)
     {
-#pragma omp section
+        if (j != k)
         {
-            sub1.row(0) = system.row(k);
-            for (int j = 0; j < N; ++j)
-            {
-                if (j != k)
-                {
-                    sub1.row(1) = system.row(j);
-                    d1 += (*f)(sub1) *f->drift_force(sub1, 0, dim_index);
-                }
-            }
-        }
-#pragma omp section
-        {
-            sub2.row(1) = system.row(k);
-            for (int j = 0; j < N; ++j)
-            {
-                if (j != k)
-                {
-                    sub2.row(0) = system.row(j);
-                    d2 += (*f)(sub2) *f->drift_force(sub2, 1, dim_index);
-                }
-            }
+            sub.row(1) = system.row(j);
+            drift += (*f)(sub) *f->drift_force(sub, 0, dim_index);
         }
     }
-    return (d1 + d2) / (*this)(system);
+
+    sub.row(1) = system.row(k);
+    for (int j = 0; j < N; ++j)
+    {
+        if (j != k)
+        {
+            sub.row(0) = system.row(j);
+            drift += (*f)(sub) *f->drift_force(sub, 1, dim_index);
+        }
+    }
+    return drift / (*this)(system);
 }
 
 Real SumPooling::laplacian(const System& system)
 {
-    const int N  = system.rows();
-    Real      l1 = 0;
-    Real      l2 = 0;
-    Real      d1 = 0;
-    Real      d2 = 0;
-    System    sub1(2, system.cols());
-    System    sub2(2, system.cols());
+    const int N       = system.rows();
+    Real      res     = 0;
+    Real      divisor = 0;
+    System    sub(2, system.cols());
     for (int i = 0; i < N; ++i)
     {
-#pragma omp parallel sections
+        sub.row(0) = system.row(i);
+        for (int j = 0; j < N; ++j)
         {
-#pragma omp section
+            if (i != j)
             {
-                sub1.row(0) = system.row(i);
-                for (int j = 0; j < i; ++j)
-                {
-                    sub1.row(1) = system.row(j);
-                    Real eval   = (*f)(sub1);
-                    l1 += eval * f->laplacian(sub1);
-                    d1 += eval;
-                }
-            }
-#pragma omp section
-            {
-                sub2.row(0) = system.row(i);
-                for (int j = i + 1; j < N; ++j)
-                {
-                    sub2.row(1) = system.row(j);
-                    Real eval   = (*f)(sub2);
-                    l2 += eval * f->laplacian(sub2);
-                    d2 += eval;
-                }
+                sub.row(1) = system.row(j);
+                Real eval  = (*f)(sub);
+                res += eval * f->laplacian(sub);
+                divisor += eval;
             }
         }
     }
-    return (l1 + l2) / (d1 + d2);
+    return res / divisor;
 }
