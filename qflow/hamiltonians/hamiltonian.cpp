d61cd72e1060509adfef0e7f3d93675c6a22901f
Bendik Samseth <b.samseth@gmail.com>
2019-05-12 19:20:27 +0300

    Verbose training also outputs parameters
    for now at least, probably to much once the networks are used again

--- qflow/hamiltonians/hamiltonian.cpp
+++ qflow/hamiltonians/hamiltonian.cpp
@@ -163,7 +163,8 @@ void Hamiltonian::optimize_wavefunction(Wavefunction& psi,
         {
             Real E_mean = local_energy(sampler, psi, sample_points);
             if (mpiutil::get_rank() == 0)
-                printf("Iteration %d: <E> = %g\n", iteration, E_mean);
+                std::cout << "Iteration " << iteration << ": <E> = " << E_mean
+                          << ": parameters = " << psi.get_parameters() << '\n';
         }
     }
 }
