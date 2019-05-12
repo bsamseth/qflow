2a57d4b9bfa39a2f6bea17a681682ea11f489380
Bendik Samseth <b.samseth@gmail.com>
2019-05-12 14:23:35 +0300

    Clean up helium sampler
    TODO: Fix initializer to be more flexible, ref issue #30

--- qflow/samplers/heliumsampler.cpp
+++ qflow/samplers/heliumsampler.cpp
@@ -6,59 +6,40 @@ HeliumSampler::HeliumSampler(const System& system,
                              Wavefunction& psi,
                              Real          step,
                              Real          box_size)
-    : Sampler(system, psi, step), L(box_size), rho(system.rows() / (L * L * L))
+    : Sampler(system, psi, step), L(box_size)
 {
     initialize_system();
 }
 
 void HeliumSampler::initialize_system()
 {
-    int    ncx       = 3;
-    int    ncy       = 3;
-    int    ncz       = 3;
-    double basisx[4] = {0., 0.5, 0.0, 0.5};
-    double basisy[4] = {0., 0.5, 0.5, 0.0};
-    double basisz[4] = {0., 0.0, 0.5, 0.5};
+    int  ncx       = 3;
+    int  ncy       = 3;
+    int  ncz       = 3;
+    Real basisx[4] = {0., 0.5, 0.0, 0.5};
+    Real basisy[4] = {0., 0.5, 0.5, 0.0};
+    Real basisz[4] = {0., 0.0, 0.5, 0.5};
 
-    int    i, j, k, ll;
-    double side, xd, yd, zd;
-    int    index;
+    const int  N    = _system_old.rows();
+    const Real side = std::pow(4. * L * L * L / N, 1. / 3.);
 
-    side = std::pow(4. / rho, 1. / 3.);
-
-    // printf("elementary cell side: %10.5f\n", side);
-
-    index = 0;
-    for (i = 0; i < ncx; i++)
-    {
-        for (j = 0; j < ncy; j++)
-        {
-            for (k = 0; k < ncz; k++)
-            {
-                for (ll = 0; ll < 4; ll++)
+    int index = 0;
+    for (int i = 0; i < ncx; i++)
+        for (int j = 0; j < ncy; j++)
+            for (int k = 0; k < ncz; k++)
+                for (int ll = 0; ll < 4; ll++)
                 {
                     _system_old(index, 0) = ((double) i + basisx[ll]) * side;
                     _system_old(index, 1) = ((double) j + basisy[ll]) * side;
                     _system_old(index, 2) = ((double) k + basisz[ll]) * side;
                     index++;
                 }
-            }
-        }
-    }
 
-    for (j = 0; j < _system_old.rows(); j++)
-    {
-        xd                = _system_old(j, 0);
-        _system_old(j, 0) = xd - L * std::rint(xd / L);
-        yd                = _system_old(j, 1);
-        _system_old(j, 1) = yd - L * std::rint(yd / L);
-        zd                = _system_old(j, 2);
-        _system_old(j, 2) = zd - L * std::rint(zd / L);
-        // printf("%10.5f  %10.5f   %10.5f\n",
-        //        _system_old(j, 0),
-        //        _system_old(j, 1),
-        //        _system_old(j, 2));
-    }
+    // Make sure all point are mapped into the simulation box.
+    for (int j = 0; j < _system_old.rows(); ++j)
+        for (int k = 0; k < _system_old.cols(); ++k)
+            _system_old(j, k) -= L * std::rint(_system_old(j, k) / L);
+
     _system_new = _system_old;
     _psi_old    = (*_wavefunction)(_system_old);
 }
--- qflow/samplers/heliumsampler.hpp
+++ qflow/samplers/heliumsampler.hpp
@@ -9,26 +9,8 @@
 class HeliumSampler : public Sampler
 {
 protected:
-    /*
-      From Sampler:
-
-      const Real    _step;
-      Wavefunction* _wavefunction;
-
-      System _system_old;
-      System _system_new;
-      Real   _psi_old;
-      Real   _psi_new;
-      long   _accepted_steps   = 0;
-      long   _total_steps      = 0;
-      int    _particle_to_move = 0;
-
-      void prepare_for_next_run();
-    */
-
 public:
     const Real L;
-    const Real rho;
 
     HeliumSampler(const System& system, Wavefunction& psi, Real step, Real box_size);
 
