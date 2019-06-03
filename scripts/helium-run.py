49209a8f6cc194537e06dbce599e90297a216225
Bendik Samseth <b.samseth@gmail.com>
2019-06-03 15:48:09 +0200

    Update Helium run script with good settings
    Obtains a energy of -6.8, vs -6.7 without the Dnn. Statistically significant
    even using Blocking based standard error. Not the biggest improvement, but
    still something.

--- scripts/helium-run.py
+++ scripts/helium-run.py
@@ -40,32 +40,32 @@ mpiprint(f"P = {P}, D = {D}, rho = {rho}, L = {L}")
 
 H = LennardJones(L)
 mcmillian = JastrowMcMillian(5, 2.965, L)
-layers = [
-    DenseLayer(P * D, 144, activation=tanh, scale_factor=0.001),
-    DenseLayer(144, 36, activation=tanh),
-    DenseLayer(36, 1, activation=exponential),
-]
-dnn = Dnn()
-for l in layers:
-    dnn.add_layer(l)
-mcmillian_fixed = FixedWavefunction(mcmillian)
-psi_total = WavefunctionProduct(mcmillian_fixed, dnn)
-psi = InputSorter(psi_total)
-# psi = mcmillian
+# layers = [
+#     DenseLayer(P * D, 144, activation=tanh, scale_factor=0.001),
+#     DenseLayer(144, 36, activation=tanh),
+#     DenseLayer(36, 1, activation=exponential),
+# ]
+# dnn = Dnn()
+# for l in layers:
+#     dnn.add_layer(l)
+# mcmillian_fixed = FixedWavefunction(mcmillian)
+# psi_total = WavefunctionProduct(mcmillian_fixed, dnn)
+# psi = InputSorter(psi_total)
+psi = mcmillian
 sampler = HeliumSampler(system, psi, 0.5, L)
 sampler.thermalize(10000)
 mpiprint(f"AR: {sampler.acceptance_rate}")
 
-optimizer = AdamOptimizer(len(psi.parameters))
+optimizer = AdamOptimizer(len(psi.parameters), 0.0001)
 
 
-steps = 1000
+steps = 0
 t_average = 0
 E_training = []
 b_training = []
 for _ in range(steps):
     t0 = time.time()
-    H.optimize_wavefunction(psi, sampler, 50, 1000, optimizer, 0, False)
+    H.optimize_wavefunction(psi, sampler, 50, 1000, optimizer, 0.00001, False)
     t1 = time.time() - t0
     t_average = (t_average * _ + t1) / (_ + 1)
     E = H.local_energy_array(sampler, psi, 5000) / P
@@ -92,14 +92,14 @@ for _ in range(steps):
 
 # psi.parameters = np.mean(b_training[-steps//10:], keepdims=True)
 
-points = 2 ** 21
+points = 2 ** 22
 t0 = time.time()
 H.local_energy_array(sampler, psi, 500)
 t1 = time.time() - t0
 eta = timedelta(seconds=round(t1 / 500 * points))
 mpiprint(f"Calculating final energy - ETA {eta}")
 
-energies = np.array(H.local_energy_array(sampler, psi, points)) / P
+energies = H.local_energy_array(sampler, psi, points) / P
 
 if MPI.COMM_WORLD.rank == 0:
     np.savetxt(
