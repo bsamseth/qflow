63e73e8be13e23db859beb39445a283f19a8735b
Bendik Samseth <b.samseth@gmail.com>
2019-06-09 18:48:21 +0200

    Update MPI speed test with results from abel login node

--- writing/scripts/mpi-speed-test.py
+++ writing/scripts/mpi-speed-test.py
@@ -4,34 +4,37 @@ from tqdm import tqdm
 from os import system
 from multiprocessing import cpu_count
 
-program = """
-from time import time
-from qflow.mpi import mpiprint
-from qflow.hamiltonians import HarmonicOscillator
-from qflow.wavefunctions import SimpleGaussian
-from qflow.samplers import MetropolisSampler
-H=HarmonicOscillator()
-psi=SimpleGaussian()
-s=MetropolisSampler([[0]],psi,1)
-t0 = time()
-for _ in range(20):
-    H.local_energy(s,psi,1000000)
-mpiprint((time() - t0) / 10)
-"""
+# program = """
+# from time import time
+# from qflow.mpi import mpiprint
+# from qflow.hamiltonians import HarmonicOscillator
+# from qflow.wavefunctions import SimpleGaussian
+# from qflow.samplers import MetropolisSampler
+# H=HarmonicOscillator()
+# psi=SimpleGaussian()
+# s=MetropolisSampler([[0]],psi,1)
+# t0 = time()
+# for _ in range(20):
+#     H.local_energy(s,psi,1000000)
+# mpiprint((time() - t0) / 10)
+# """
 
-cpus = np.arange(1, cpu_count() + 1)
-for n in tqdm(cpus):
-    system(
-        f"mpiexec -n {n} --oversubscribe pipenv run python -c '{program}' >> mpi-speed-test-results.dat"
-    )
+# cpus = np.arange(1, cpu_count() + 1)
+# for n in tqdm(cpus):
+#     system(
+#         f"mpiexec -n {n} --oversubscribe pipenv run python -c '{program}' >> mpi-speed-test-results.dat"
+#     )
 
-times = np.loadtxt("mpi-speed-test-results.dat")
-print(times)
-system("rm mpi-speed-test-results.dat")
+# Following data produced by running the above script on a login node at abel, discarding results for n > 5 (unstable access to processors)
+times = np.array([0.40548885, 0.21678371, 0.15273976, 0.11579297, 0.09907522])
+cpus = np.arange(1, len(times) + 1)
 
-plt.plot(cpus, 1_000_000 / times, label="Actual ops/sec")
-plt.plot(cpus, 1_000_000 / times[0] * cpus, "--", label="Linear speedup")
+plt.plot(cpus, times[0] / times, label="Actual")
+plt.plot(cpus, cpus, "--", label="Ideal speedup")
 plt.legend()
+plt.xlabel("Cores")
+plt.ylabel("Iterations per second")
+
 import matplotlib2tikz
 
 matplotlib2tikz.save(__file__ + ".tex")
