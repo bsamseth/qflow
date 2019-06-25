85d73d5d9b8278a8e5f3dd799c66683e15011093
Bendik Samseth <b.samseth@gmail.com>
2019-06-25 12:38:32 +0200

    Add RBM QD results

--- writing/scripts/QD-rbm.py
+++ writing/scripts/QD-rbm.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib2tikz
 
-from qflow.wavefunctions import RBMWavefunction
+from qflow.wavefunctions import RBMWavefunction, InputSorter
 from qflow.hamiltonians import CoulombHarmonicOscillator
-from qflow.samplers import ImportanceSampler
+from qflow.samplers import ImportanceSampler, MetropolisSampler
 from qflow.optimizers import AdamOptimizer
 from qflow.training import train, EnergyCallback, SymmetryCallback, ParameterCallback
 from qflow.statistics import compute_statistics_for_series, statistics_to_tex
@@ -19,7 +19,7 @@ def plot_training(energies, symmetries, parameters):
     eax.axhline(y=3, label="Exact", linestyle="--", color="k", alpha=0.5)
     eax.legend()
 
-    pax.semilogx(np.asarray(parameters))
+    pax.plot(np.asarray(parameters))
     pax.set_xlabel(r"% of training")
 
     matplotlib2tikz.save(__file__ + ".tex")
@@ -41,32 +41,40 @@ P, D, N = 2, 2, 4  # Particles, dimensions, hidden nodes.
 system = np.empty((P, D))
 H = CoulombHarmonicOscillator()
 psi = RBMWavefunction(P * D, N)
+psi_sorted = InputSorter(psi)
 psi_sampler = ImportanceSampler(system, psi, step_size=0.1)
+psi_sorted_sampler = ImportanceSampler(system, psi_sorted, step_size=0.1)
 
-psi_energies = EnergyCallback(samples=1000)
-psi_symmetries = SymmetryCallback(samples=500)
+psi_energies = EnergyCallback(samples=1000000, verbose=True)
+psi_symmetries = SymmetryCallback(samples=1000000)
 psi_parameters = ParameterCallback()
 
 train(
     psi,
     H,
     psi_sampler,
-    iters=1000,
-    samples=2000,
+    iters=40000,
+    samples=1000,
     gamma=0.001,
-    optimizer=AdamOptimizer(len(psi.parameters)),
+    optimizer=AdamOptimizer(len(psi.parameters), 0.005),
     call_backs=(psi_energies, psi_symmetries, psi_parameters),
 )
 
 mpiprint("Training complete")
 
 
+psi_sorted_sampler.thermalize(100000);
+mpiprint(f"Sorted sampler acceptance rate: {psi_sorted_sampler.acceptance_rate}")
+
 stats = [
     compute_statistics_for_series(
-        H.local_energy_array(psi_sampler, psi, 2 ** 12), method="blocking"
+        H.local_energy_array(psi_sampler, psi, 2 ** 23), method="blocking"
+    ),
+    compute_statistics_for_series(
+        H.local_energy_array(psi_sorted_sampler, psi_sorted, 2 ** 23), method="blocking"
     )
 ]
-labels = [r"$\psi_{RBM}$"]  # , r"$\psi_{PJ}$"]
+labels = [r"$\psi_{RBM}$", r"$\psi_{SRBM}$"]
 
 mpiprint(stats, pretty=True)
 mpiprint(statistics_to_tex(stats, labels, filename=__file__ + ".table.tex"))
